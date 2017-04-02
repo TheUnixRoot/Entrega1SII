1b873f1c20fc946a037bf7e8ebdf92d91a11f17d
juanp <juanp@DESKTOP-6H90V9P>
2017-04-02 22:44:22 +0200

    Correciones menores 1

--- prEntrega1/src/main/java/grupoj/prentrega1/Administrador.java
+++ prEntrega1/src/main/java/grupoj/prentrega1/Administrador.java
@@ -5,13 +5,9 @@
  */
 package grupoj.prentrega1;
 
-import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
--- prEntrega1/src/main/java/grupoj/prentrega1/Periodista.java
+++ prEntrega1/src/main/java/grupoj/prentrega1/Periodista.java
@@ -5,13 +5,9 @@
  */
 package grupoj.prentrega1;
 
-import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
