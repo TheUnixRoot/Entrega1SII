72e49f4c175ed130d53216f9e870059249322a81
juanp <juanp@DESKTOP-6H90V9P>
2017-04-23 15:36:18 +0200

    Creacion de la clase modelo para imprimir eventos
    Ademas de las modificaciones necesarias en el pom
    y en el mock de persistencia.

---
+++ entregaJSF/src/main/java/grupoj/entregajsf/toPDF/PdfCreator.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupoj.entregajsf.toPDF;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import grupoj.prentrega1.Evento;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author juanp
+ */
+public class PdfCreator {
+    
+    private static Font catFont = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
+    private static Font redFont = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL, BaseColor.RED);
+    private static Font subFont = new Font(Font.FontFamily.HELVETICA, 16, Font.BOLD);
+    private static Font smallBold = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+
+    private String path;
+    
+    public PdfCreator(Evento ev) {
+        Document doc = new Document();
+        path = "/temp/docs/" 
+                    + (new SimpleDateFormat("ddMMyyyy_HHmmss")).format(new Date());
+        try {
+            PdfWriter.getInstance(doc, new FileOutputStream(path));
+            doc.open();
+            addEvent(doc, ev);
+            doc.close();
+        } catch (FileNotFoundException | DocumentException ex) {
+            Logger.getLogger(PdfCreator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static void addEvent(Document doc, Evento ev) throws DocumentException {
+        doc.addTitle(ev.getNombre());
+        Paragraph evento = new Paragraph();
+        evento.add(new Paragraph(ev.getNombre(), catFont));
+        evento.add(new Paragraph(" "));
+        evento.add(new Paragraph(ev.getDescripcion(), subFont));
+        evento.add(new Paragraph(" "));
+        evento.add(new Paragraph(ev.getDonde_comprar(), smallBold));
+        evento.add(new Paragraph(" "));
+        doc.add(evento);
+    }
+    
+    public String getPath() {
+        return path;
+    }
+}
--- entregaJSF/src/main/java/mockingBeans/PersistenceMock.java
+++ entregaJSF/src/main/java/mockingBeans/PersistenceMock.java
@@ -6,12 +6,17 @@
 package mockingBeans;
 
 import grupoj.prentrega1.Administrador;
+import grupoj.prentrega1.Evento;
+import grupoj.prentrega1.Geolocalizacion;
+import grupoj.prentrega1.Lugar;
 import grupoj.prentrega1.Periodista;
 import grupoj.prentrega1.TipoNotificacion;
 import grupoj.prentrega1.Usuario;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 
@@ -24,7 +29,7 @@ import javax.enterprise.context.RequestScoped;
 public class PersistenceMock implements Serializable {
     
     private List<Usuario> listaUsuarios;
-    
+    private List<Evento> listaEventos;
     
     /**
      * Creates a new instance of PersistenceMock
@@ -57,6 +62,27 @@ public class PersistenceMock implements Serializable {
         usr.setNombre("administradorcito");
         usr.setMultimedia("none");
         listaUsuarios.add(usr);
+        
+        Lugar lug = new Lugar();
+        Geolocalizacion geo = new Geolocalizacion();
+        
+        geo.setDireccion("Bulevar Luis Pasteur, 35, campus de Teatinos, 29071, Malaga");
+        
+        lug.setBorrado(false);
+        lug.setNombre("ETSI Informatica");
+        lug.setGeolocalizacion(geo);
+        lug.setGeolocalizacion(geo);
+        geo.setLugar(lug);
+        
+        Evento ev = new Evento();
+        ev.setNombre("Evento 1");
+        ev.setBorrado(false);
+        ev.setValidado(true);
+        ev.setFecha(new Date());
+        ev.setOcurre_in(lug);
+        
+        listaEventos.add(ev);
+        
     }
 
     public List<Usuario> getListaUsuarios() {
@@ -65,6 +91,14 @@ public class PersistenceMock implements Serializable {
 
     public void setListaUsuarios(List<Usuario> listaUsuarios) {
         this.listaUsuarios = listaUsuarios;
+    }
+
+    public List<Evento> getListaEventos() {
+        return listaEventos;
+    }
+
+    public void setListaEventos(List<Evento> listaEventos) {
+        this.listaEventos = listaEventos;
     }
     
 }
