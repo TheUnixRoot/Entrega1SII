19e5805fa3b5bf6b2750fb6080456be859bc14c0
anaes <anaes@LAPTOP-G55VGU8T>
2017-04-27 17:54:38 +0200

    BuscarEvento html hecho, no buscar porque necesita consulta, revisar lugares que se muestran en la lista.

--- entregaJSF/src/main/java/mockingBeans/PersistenceMock.java
+++ entregaJSF/src/main/java/mockingBeans/PersistenceMock.java
@@ -6,7 +6,7 @@
 package mockingBeans;
 
 import grupoj.prentrega1.TipoNotificacion;
-import grupoj.prentrega1.Usuario;
+import grupoj.prentrega1.*;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -22,7 +22,9 @@ import javax.enterprise.context.RequestScoped;
 public class PersistenceMock implements Serializable {
     
     private List<Usuario> listaUsuarios;
-    
+    private List<Evento> listaEventos;
+    private Lugar lugar1;
+    private List<Tag> listaTags;
     
     /**
      * Creates a new instance of PersistenceMock
@@ -37,6 +39,37 @@ public class PersistenceMock implements Serializable {
         usr.setNombre("normalito");
         usr.setMultimedia("none");
         listaUsuarios.add(usr);
+        
+        
+        Geolocalizacion geo = new Geolocalizacion();
+        geo.setDireccion("NombreCalle");
+        geo.setLugar(lugar1);
+        
+        lugar1 = new Lugar();
+        lugar1.setNombre("Recinto ferial");
+        lugar1.setGeolocalizacion(geo);
+        
+        Tag tag1 = new Tag();
+        tag1.setTexto("Música");
+        Tag tag2 = new Tag();
+        tag2.setTexto("Teatro");
+        listaTags = new ArrayList<Tag>();
+        listaTags.add(tag1);
+        listaTags.add(tag2);
+        
+        listaEventos = new ArrayList<>();
+        Evento e = new Evento();
+        e.setNombre("Feria Málaga");
+        
+        e.setBorrado(false);
+        e.setValidado(true);
+        e.setDescripcion("Feria de malaga 2017");
+        e.setPrecio(20);
+        e.setDonde_comprar("www.malaga.com");
+        e.setTagged_by(listaTags);
+        e.setOcurre_in(lugar1);
+        listaEventos.add(e);
+        
     }
 
     public List<Usuario> getListaUsuarios() {
@@ -45,6 +78,13 @@ public class PersistenceMock implements Serializable {
 
     public void setListaUsuarios(List<Usuario> listaUsuarios) {
         this.listaUsuarios = listaUsuarios;
+    }
+    public List<Evento> getListaEventos() {
+        return listaEventos;
+    }
+
+    public void setListaEventos(List<Evento> listaeventos) {
+        this.listaEventos = listaeventos;
     }
     
 }
