8b4ba6901ecc3a1b99eae5a8472c5d5d1d84b28b
juanp <juanp@DESKTOP-6H90V9P>
2017-04-27 18:00:10 +0200

    Revert "La que vamos a liar xD"
    This reverts commit d239df4a3bbab23ad767dac2b36f50b808d2bd07.

---
+++ entregaJSF/src/main/java/grupoj/entregajsf/backingBeans/Crud_usuariosBean.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupoj.entregajsf.backingBeans;
+
+import grupoj.prentrega1.Usuario;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import mockingBeans.PersistenceMock;
+
+/**
+ *
+ * @author juanp
+ */
+@Named(value = "crud_usuariosBean")
+@RequestScoped
+public class Crud_usuariosBean {
+    
+    @Inject
+    private PersistenceMock persistencia;
+    private Iterable<Usuario> usuarios;
+    private Iterable<Usuario> periodistas;
+    private Iterable<Usuario> administradores;
+    
+    /**
+     * Creates a new instance of Crud_usuariosBean
+     */
+    @PostConstruct
+    public void init() {
+        usuarios = persistencia.getListaUsuarios();
+    }
+
+    public Iterable<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Iterable<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public Iterable<Usuario> getPeriodistas() {
+        return periodistas;
+    }
+
+    public void setPeriodistas(Iterable<Usuario> periodistas) {
+        this.periodistas = periodistas;
+    }
+
+    public Iterable<Usuario> getAdministradores() {
+        return administradores;
+    }
+
+    public void setAdministradores(Iterable<Usuario> administradores) {
+        this.administradores = administradores;
+    }
+    
+}
