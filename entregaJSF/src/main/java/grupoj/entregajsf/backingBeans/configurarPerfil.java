fbc06f6785edc10fa95d15e57d3eabcedb130767
anaes <anaes@LAPTOP-G55VGU8T>
2017-04-28 09:47:35 +0200

    Configurar perfil casi hecho, falta: imagenes y saber qué usuario utilizar.

---
+++ entregaJSF/src/main/java/grupoj/entregajsf/backingBeans/configurarPerfil.java
@@ -0,0 +1,113 @@
+package grupoj.entregajsf.backingBeans;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import grupoj.prentrega1.*;
+import java.util.List;
+import mockingBeans.PersistenceMock;
+
+/**
+ *
+ * @author anaes
+ */
+@Named(value = "configurarPerfil")
+@RequestScoped
+public class configurarPerfil {
+    private PersistenceMock persistencia = new PersistenceMock();
+    private List<Usuario> listaUsuario;
+    private Usuario usuario;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String contrasenia;
+    private String telefono;
+    private Date fechaNacimiento;
+    
+    /**
+     * Creates a new instance of configurarPerfil
+     */
+    public configurarPerfil() {
+        listaUsuario = persistencia.getListaUsuarios();
+        usuario = listaUsuario.get(0);
+        nombre = usuario.getNombre();
+        apellidos = usuario.getApellidos();
+        email = usuario.getEmail();
+        contrasenia = usuario.getPassword();
+        telefono = usuario.getTelefono();
+        fechaNacimiento = usuario.getFechaNacimiento();
+                
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombreU(){
+        return "Pepe";
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    public String configurar(){
+        usuario.setNombre(nombre);
+        usuario.setApellidos(apellidos);
+        usuario.setEmail(email);
+        usuario.setPassword(contrasenia);
+        usuario.setTelefono(telefono);
+        usuario.setFechaNacimiento(fechaNacimiento);
+        listaUsuario.remove(0);
+        listaUsuario.add(usuario);
+        persistencia.setListaUsuarios(listaUsuario);
+        
+        return "index.html";
+    }
+    
+}
