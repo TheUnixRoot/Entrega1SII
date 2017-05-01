1f841a7d85db19257b4ad577dae41fc51b5a4146
anaes <anaes@LAPTOP-G55VGU8T>
2017-05-01 18:40:15 +0200

    verEvento listo, suponiendo que lo demás de evento está bien

--- entregaJSF/src/main/java/grupoj/entregajsf/backingBeans/verEvento.java
+++ entregaJSF/src/main/java/grupoj/entregajsf/backingBeans/verEvento.java
@@ -1,3 +1,5 @@
+package grupoj.entregajsf.backingBeans;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,9 +7,11 @@
  */
 
 import grupoj.prentrega1.Evento;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import mockingBeans.PersistenceMock;
 
@@ -23,12 +27,24 @@ public class verEvento {
     @Inject
     PersistenceMock persistencia;
     private Evento evento;
+    private long id;
     
     
-    
-    @PostConstruct
-    public void init() {
-        evento = this.persistencia.getListaEventos().get(0);
+        @PostConstruct
+        public void init() {
+        Map<String, String> req = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        this.setId(Long.parseLong(req.get("id")));
+        Evento ev = new Evento();
+        ev.setId(id);
+        System.out.println(id);
+        if ( this.persistencia.getListaEventos().contains(ev) ) 
+            this.evento = this.persistencia.getListaEventos()
+                    .get(
+                            this.persistencia.getListaEventos().indexOf(ev)
+                    );
+        else
+            this.evento = null;
+        ev = null;
     }
     
     /**
@@ -51,6 +67,14 @@ public class verEvento {
 
     public void setEvento(Evento evento) {
         this.evento = evento;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
     
     
