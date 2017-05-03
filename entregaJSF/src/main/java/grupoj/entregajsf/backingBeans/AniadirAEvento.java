73e063ed2397b389baf3bc21ce313ac927793a47
David <davidaguilera91@uma.es>
2017-05-03 16:42:12 +0200

    Vista de añadir comentario hecha.

--- entregaJSF/src/main/java/grupoj/entregajsf/backingBeans/AniadirAEvento.java
+++ entregaJSF/src/main/java/grupoj/entregajsf/backingBeans/AniadirAEvento.java
@@ -5,20 +5,78 @@
  */
 package grupoj.entregajsf.backingBeans;
 
+import grupoj.entregajsf.controlSesion.ControlAutorizacion;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import mockingBeans.PersistenceMock;
+import org.primefaces.model.UploadedFile;
 
 /**
  *
  * @author David
  */
-@Named(value = "añadirAEvento")
+@Named(value = "aniadirAEvento")
 @Dependent
 public class AniadirAEvento {
+    @Inject
+    private PersistenceMock persistencia;
+    @Inject
+    private ControlAutorizacion ctrAut;
+    
+    private Integer valEvento;
+    private Integer valLugar;
+    
+    private String comEvento;
+    private String comLugar;
+    
+    private UploadedFile fEvento;
+    private UploadedFile fLugar;
+    
+    public Integer getValEvento() {
+        return valEvento;
+    }
 
-    /**
-     * Creates a new instance of AñadirAEvento
-     */
+    public void setValEvento(Integer valEvento) {
+        this.valEvento = valEvento;
+    }
+
+    public Integer getValLugar() {
+        return valLugar;
+    }
+
+    public void setValLugar(Integer valLugar) {
+        this.valLugar = valLugar;
+    }
+
+    public String getComEvento() {
+        return comEvento;
+    }
+
+    public void setComEvento(String comEvento) {
+        this.comEvento = comEvento;
+    }
+
+    public String getComLugar() {
+        return comLugar;
+    }
+
+    public void setComLugar(String comLugar) {
+        this.comLugar = comLugar;
+    }
+    
+    
+    
+    public String añadirComentarioEvento() {
+        
+        return null;
+    }
+    
+    public String añadirComentarioLugar() {
+        
+        return null;
+    }
+    // Constructor
     public AniadirAEvento() {
     }
     
